a045df4df257a0e7e7cb13b08fbaa2a05bdcaf43
Albert Sun [I] <alsun@tesla.com>
2024-09-07 17:26:07 -0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [words, setWords] = useState("The quick brown fox jumped over the lazy dog")
+  const [index, setIndex] = useState(0)
+  const [arr, setArr] = useState(new Array(words.length).fill(0))
+  const [cursor, setCursor] = useState([0, 0])
+
+  const update = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const key = e.key
+    console.log(arr)
+    console.log(key, index)
+    if (key.length > 1) {
+      return
+    }
+    if (key == words[index]) {
+      let n = [...arr]
+      n[index] = 1
+      setArr(n)
+      setIndex(index + 1)
+    }
+    else {
+      let n = [...arr]
+      n[index] = 2
+      setArr(n)
+      setIndex(index + 1)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className='flex flex-row'>
+        {words && words.split("").map((char, i) => {
+          return <div className={`text-5xl ${arr[i] === 0 ? "text-gray-500" : arr[i] === 1 ? "text-green-500" : "text-red-700"}`} id={"" + i}>{char}</div>
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <input type='text' onKeyDown={(e) => update(e)}></input>
+    </div>
+
   )
 }
 

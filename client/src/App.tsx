c48ba615e83753075939963d9a5d8ade9a1815e0
Albert Sun <albertsun0@gmail.com>
2024-09-10 23:02:35 -0700

    idk

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,95 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 
+type charcter = {
+  letter: string;
+  index: number;
+};
+
+type word = {
+  word: charcter[];
+  extra?: string;
+};
+
 function App() {
-  const [words, setWords] = useState(
-    "The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. "
-  );
+  const [words, setWords] = useState<word[]>([]);
+  const [string, setString] = useState("");
   const [index, setIndex] = useState(0);
   const [arr, setArr] = useState(new Array(words.length).fill(0));
   const [cursor, setCursor] = useState([0, 0]);
+
+  // Run when clinet recieves prompt from server
+  const updatePrompt = (prompt: string) => {
+    setIndex(0);
+    setString(prompt);
+    setArr(new Array(prompt.length).fill(0));
+
+    // let split = prompt.split(" ");
+    let words: word[] = [];
+    let word: charcter[] = [];
+    for (let i = 0; i < prompt.length; i++) {
+      if (prompt[i] == " ") {
+        words.push({ word: word });
+        words.push({ word: [{ letter: " ", index: i }] });
+        word = [];
+      } else {
+        let character = {
+          letter: "" + prompt[i],
+          index: i,
+        };
+        word.push(character);
+      }
+    }
+    if (word.length > 0) {
+      words.push({ word: word });
+    }
+    console.log(words);
+    setWords(words);
+  };
 
   const update = (e: React.KeyboardEvent<HTMLInputElement>) => {
     const key = e.key;
     console.log(arr);
     console.log(key, index);
+    if (key == " ") {
+      if (index == 0) return;
+      if (string[index - 1] == " ") return;
+      if (string[index] == " ") {
+        let n = [...arr];
+        n[index] = 1;
+        setArr(n);
+        setIndex(index + 1);
+        return;
+      }
+      let i = index + 1;
+      console.log(string[i]);
+      while (i < string.length && string[i] != " ") {
+        console.log(string[i]);
+        i++;
+      }
+      setIndex(i + 1);
+      return;
+    }
     if (key.length > 1) {
       if (key == "Backspace") {
         if (index == 0) return;
+        if (arr[index - 1] == 0) {
+          let i = index;
+          while (i > 0 && arr[i] == 0) {
+            i -= 1;
+          }
+          setIndex(i + 1);
+          return;
+        }
         let n = [...arr];
         n[index - 1] = 0;
         setArr(n);
         setIndex(index - 1);
       }
-    } else if (key == words[index]) {
+    } else if (key == string[index]) {
+      if (index == string.length - 2) {
+        console.log("success");
+      }
       let n = [...arr];
       n[index] = 1;
       setArr(n);
@@ -39,38 +107,41 @@ function App() {
       .getElementById("" + index)
       ?.getBoundingClientRect();
     if (element) {
-      setCursor([element.x, element.y]);
+      setCursor([element.x - 2, element.y]);
     }
-  }, [index]);
+  }, [index, prompt]);
+
+  useEffect(() => {
+    const sentence = "dog" + " ";
+    updatePrompt(sentence);
+  }, []);
+
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center w-screen h-screen bg-[#333437] p-28 px-32 justify-center">
       <div
-        className="absolute w-1 h-12 bg-gray-300 transition-all duration-100 ease-out"
+        className="absolute w-1 h-12 bg-[#e2b711] transition-all duration-100 ease-out"
         style={{ top: cursor[1], left: cursor[0] }}
       ></div>
-      <div className="flex flex-row flex-wrap w-1/2">
+      <p>
         {arr &&
           arr.map((i, index) => {
             return (
-              <div
+              <span
                 className={`text-5xl ${
                   i === 0
-                    ? "text-gray-500"
+                    ? "text-[#606366]"
                     : i === 1
-                    ? "text-green-500"
-                    : "text-red-700"
+                    ? "text-[#d1d0c5]"
+                    : "text-[#ca4754]"
                 }`}
                 id={"" + index}
               >
-                {words[index] == " " ? (
-                  <div className="w-4"> </div>
-                ) : (
-                  <div>{words[index]}</div>
-                )}
-              </div>
+                {string[index]}
+              </span>
             );
           })}
-      </div>
+      </p>
+
       <input type="text" onKeyDown={(e) => update(e)} autoFocus></input>
     </div>
   );
